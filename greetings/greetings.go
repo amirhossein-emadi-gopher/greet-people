55d6a79da114b3f147f93a923bcbf2720db8c437
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-10-22 01:38:14 -0700

    ✨ feat: add Hello function for personalized greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,1 +1,9 @@
 package greetings
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi %s. Welcome!", name)
+	return message
+}
